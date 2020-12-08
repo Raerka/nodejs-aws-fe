0e62157d2c8d7daed02231d3e9dba5d5488dcde4
Andrei Kasmykou <andrei_kasmykou@epam.com>
2020-12-08 07:52:11 +0300

    add cart service

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://94ynxyaj90.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://ehohru5de4.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://94ynxyaj90.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://raerka-cart-api.eu-west-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
